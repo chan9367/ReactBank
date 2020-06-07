f7d6a99c955a00312df596ec0c266e4b3aa449fa
Ke-vin Chan <chan9367@gmail.com>
2020-06-07 15:57:19 -0500

    up until LAB part

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-    
-    class App extends Component {
-      render() {
-        return (
-            <div className="App">
-              Hello, World!
-            </div>
-        );
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home';
+import UserProfile from './components/UserProfile';
+import LogIn from './components/Login';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'bob_loblaw',
+        memberSince: '08/23/99',
       }
     }
-    
-    export default App;
+  }
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName = logInInfo.userName
+    this.setState({currentUser: newUser})
+  }
+  render() {
+    const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} {...this.props}/>)
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+        <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
+    );
+
+    return (
+        <Router>
+          <div>
+            <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/userProfile" render={UserProfileComponent}/>
+            <Route exact path="/login" render={LogInComponent}/>
+          </div>
+        </Router>
+    );
+  }
+
+}
+
+export default App;
